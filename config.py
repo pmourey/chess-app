86610860aabee9121cc813e5479a60beaa86a2e5
Philippe Mourey <philippe@mourey.com>
2025-03-18 00:47:09 +0100

    delete api key mapbox

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     DEBUG = True
     SECRET_KEY = 'fifa 2022'
     BASE_PATH = os.path.dirname(__file__)
-    MAPBOX_API_KEY = 'pk.eyJ1IjoicG1vdXJleSIsImEiOiJjbHZ4dnJ3bGMwNDBzMmlxeXBnZDJtYzVrIn0.yxrohiV1L6c89UY6PvPHGw'
     ENGINES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'engines', 'stockfish')
     LICHESS_API_URL = "https://lichess.org/api"
     LICHESS_API_TOKEN = os.getenv('LICHESS_API_TOKEN')
